1ccb4716883f40299f377755dc053cbeb45629b8
Deep Katbamna <deepmihir@gmail.com>
2022-07-12 13:46:02 +0530

    Home_page_ui Day-1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,43 +6,74 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title : Text("Yoga App")
-      ),
-      body : Column(
-        children: [
-          Padding(
-
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: Text("Yoga App")),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("1"),
-                    Text("Steak")
+                    Column(
+                      children: [Text("1"), Text("Steak")],
+                    ),
+                    Column(
+                      children: [Text("1"), Text("Steak")],
+                    ),
+                    Column(
+                      children: [Text("1"), Text("Steak")],
+                    )
                   ],
                 ),
-                 Column(
+              ),
+              Divider(
+                thickness: 5,
+                indent: 20,
+                endIndent: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("1"),
-                    Text("Steak")
+                    Text("Yoga for all"),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Container(
+                        color: Colors.blueGrey,
+                        height: 125,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Container(
+                        color: Colors.blueGrey,
+                        height: 150,
+                      ),
+                    ),
+                    Text("Yoga for all"),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Container(
+                        color: Colors.blueGrey,
+                        height: 150,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Container(
+                        color: Colors.blueGrey,
+                        height: 150,
+                      ),
+                    )
                   ],
                 ),
-                 Column(
-                  children: [
-                    Text("1"),
-                    Text("Steak")
-                  ],
-                )
-              ],
-            ),
+              )
+            ],
           ),
-          Divider(thickness: 5, indent: 20, endIndent: 20,)
-        ],
-    
-      )
-    );
+        ));
   }
 }
